c38602a90d8ce063af8fa976cf52ab39038315fb
JherBear <72480494+JherBear@users.noreply.github.com>
2025-08-21 12:23:04 -0700

    roll result bug

--- index.js
+++ index.js
@@ -22,17 +22,17 @@ function updateRollHistory(){
 function getDiceFace(rollResult){
     switch(rollResult){
         case 1:
-            return "&#9856;"
+            return "&#9856";
         case 2:
-            return "&#9857;"
+            return "&#9857";
         case 3:
-            return "&#9858;"
+            return "&#9858";
         case 4:
-            return "&#9859;"
+            return "&#9859";
         case 5:
-            return "&#9860;"
+            return "&#9860";
         case 6:
-            return "&#9861;"
+            return "&#9861";
         default:
             return "";
     }
