10c90361eb25ceac57700f195955dc18ba6a87d4
JherBear <72480494+JherBear@users.noreply.github.com>
2025-08-21 20:14:42 -0700

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const buttonEl = document.getElementById("roll-button")
+const buttonEl = document.getElementById("roll-button");
 
-const diceEl = document.getElementById("dice")
+const diceEl = document.getElementById("dice");
 
-const rollHistoryEl = document.getElementById("roll")
+const rollHistoryEl = document.getElementById("roll-history");
 
 let historyList = [];
 
@@ -10,29 +10,30 @@ function rollDice(){
     const rollResult = Math.floor(Math.random() * 6) +1;
     const diceFace = getDiceFace(rollResult);
     diceEl.innerHTML = diceFace;
-    //diceEl.textContent = diceFace;
     historyList.push(rollResult);
     updateRollHistory();
 }
 
 function updateRollHistory(){
-
+    rollHistoryEl.innerHTML = "";
+    for(let i = 0; i < historyList.length; i++) {
+    }
 }
 
 function getDiceFace(rollResult){
     switch(rollResult){
         case 1:
-            return "&#9856";
+            return "&#9856;";
         case 2:
-            return "&#9857";
+            return "&#9857;";
         case 3:
-            return "&#9858";
+            return "&#9858;";
         case 4:
-            return "&#9859";
+            return "&#9859;";
         case 5:
-            return "&#9860";
+            return "&#9860;";
         case 6:
-            return "&#9861";
+            return "&#9861;";
         default:
             return "";
     }
